99084ebafd6c7ee22f3cc53d84499ad93d25a7cf
[rauf] <[raufam@code.edu.az]>
2022-12-28 14:22:57 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace muxtelifreqem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static string checkSameDigits(int n)
+            {
+                int digit = n % 10;
+
+                while (n != 0)
+                {
+                    int current_digit = n % 10;
+                    n = n / 10;
+
+                    if (current_digit != digit)
+                    {
+                        return "Muxtelif reqemli";
+                    }
+                }
+
+                return "Eynireqemli";
+            }
+
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.Write(checkSameDigits(n));
+            }
+        }
+    }
+}
+ 
